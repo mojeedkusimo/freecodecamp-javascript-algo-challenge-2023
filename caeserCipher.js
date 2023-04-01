58854eab738c7aa0f20bf84f2226ae11174b8a42
Mojeed A. Kusimo <mkusimo90@gmail.com>
2023-04-01 03:53:27 +0100

    Complete Caser Cipher

---
+++ caeserCipher.js
@@ -0,0 +1,44 @@
+function rot13(str) {
+
+    let newStr = "";
+
+    for (let i = 0; i < str.length; i++) {
+        if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
+            let newCharCode = str.charCodeAt(i) + 13;
+            let newChar = "";
+
+            if (newCharCode > 90) {
+                let reGenCharCode = newCharCode - (90) + (64);
+                newChar = String.fromCharCode(reGenCharCode);
+                newStr += newChar;
+
+            } else {
+                newChar = String.fromCharCode(newCharCode);
+                newStr += newChar;
+            }
+
+        } else {
+            newStr += str[i];
+        }
+
+    }
+    console.log(newStr);
+    return newStr;
+  }
+  
+  rot13("SERR PBQR PNZC");
+  rot13("SERR CVMMN!");
+  rot13("SERR YBIR?");
+  rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.");  
+// One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+
+// A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus A ↔ N, B ↔ O and so on.
+
+// Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+// All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+
+// Waiting:rot13("SERR PBQR PNZC") should decode to the string FREE CODE CAMP
+// Waiting:rot13("SERR CVMMN!") should decode to the string FREE PIZZA!
+// Waiting:rot13("SERR YBIR?") should decode to the string FREE LOVE?
+// Waiting:rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") should decode to the string THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
